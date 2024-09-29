0f48984d2124267275805f20b2254464b42534b4
Angela Torro <angtorbal@alu.edu.gva.es>
2024-09-29 21:22:50 +0200

    Refactor carousel component to use page input

--- Proyecto_JobAtmosphere/frontend/src/app/shared/carousel/carousel.component.ts
+++ Proyecto_JobAtmosphere/frontend/src/app/shared/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input} from '@angular/core';
-import { CarouselDetails, CarouselHome } from 'src/app/core/models/carousel.model';
-import { Category } from 'src/app/core/models/category.model';
+import { Component, OnInit, Input } from '@angular/core';
+import {
+  CarouselDetails,
+  CarouselHome,
+} from 'src/app/core/models/carousel.model';
 import { CarouselService } from 'src/app/core/services/carousel.service';
 import { ActivatedRoute } from '@angular/router';
 import { JobService } from '../../core/services/job.service';
@@ -8,39 +10,44 @@ import { JobService } from '../../core/services/job.service';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
-
 export class CarouselComponent implements OnInit {
-
+  @Input() page!: string;
   items_carousel!: CarouselHome[];
   items_details!: CarouselDetails[];
   slug_details!: string | null;
-  page!: String;
 
-  constructor(private CarouselService: CarouselService, private jobService: JobService, private ActivatedRoute: ActivatedRoute,) {}
+  constructor(
+    private carouselService: CarouselService,
+    private jobService: JobService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.slug_details = this.ActivatedRoute.snapshot.paramMap.get('slug');
-    this.carousel_categories();
-    this.carousel_shop_details();
+    this.slug_details = this.activatedRoute.snapshot.paramMap.get('slug');
+    this.loadCarouselItems();
+  }
+
+  loadCarouselItems(): void {
+    if (this.page === 'home') {
+      this.carousel_categories();
+    } else if (this.page === 'details' && this.slug_details) {
+      this.carousel_shop_details();
+    }
   }
 
   carousel_categories(): void {
-    this.page = "categories";
-    this.CarouselService.getCarouselHome().subscribe(((data : any) => {
-      // console.log(data);
+    this.carouselService.getCarouselHome().subscribe((data: any) => {
       this.items_carousel = data.categories;
-  })) 
+    });
   }
+
   carousel_shop_details(): void {
-    if(this.slug_details){
-      this.page = "details";
-      this.CarouselService.getCarouselDetails(this.slug_details).subscribe(((data : any) => {
-        // console.log(data);
+    this.carouselService
+      .getCarouselDetails(this.slug_details)
+      .subscribe((data: any) => {
         this.items_details = data.jobs.images;
-        // console.log(this.items_details);
-    })) 
-    }
+      });
   }
 }
